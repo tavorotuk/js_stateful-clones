2fe694e5db26a230ae06798efdd434436c050d0c
Vadym Kirilkov <tav0r0tuk@gmail.com>
2025-07-02 15:31:41 +0200

    finished

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -6,8 +6,30 @@
  *
  * @return {Object[]}
  */
+
 function transformStateWithClones(state, actions) {
-  // write code here
+  const newStates = [];
+  let currentState = { ...state };
+
+  for (const action of actions) {
+    if (action.type === 'addProperties') {
+      currentState = { ...currentState, ...action.extraData };
+    } else if (action.type === 'removeProperties') {
+      const newState = { ...currentState };
+
+      for (const key of action.keysToRemove) {
+        delete newState[key];
+      }
+
+      currentState = newState;
+    } else if (action.type === 'clear') {
+      currentState = {};
+    }
+
+    newStates.push(currentState);
+  }
+
+  return newStates;
 }
 
 module.exports = transformStateWithClones;
